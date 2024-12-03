b51754d0106cbe4d4bb6d5b7135b9fd10d12a0b0
DassiKastner <3821572531@mby.co.il>
2024-12-03 18:34:13 +0200

    I added procedure of adding Attachment and Logger to DB

--- EX2/EX2/Repositories/AttachmentRepository.cs
+++ EX2/EX2/Repositories/AttachmentRepository.cs
@@ -24,14 +24,6 @@ namespace EX2.Repositories
                     connection.Open();
                     command.CommandText = "Add_Attachment";
                     command.CommandType = CommandType.StoredProcedure;
-
-                    //SqlParameter name = new SqlParameter("@AttachName", AttachName);
-                    //SqlParameter path = new SqlParameter("@AttachPath", AttachPath);
-                    //SqlParameter date = new SqlParameter("@UploadDate", UploadDate);
-
-                    //command.Parameters.Add(name);
-                    //command.Parameters.Add(path);
-                    //command.Parameters.Add(date);
                     command.Parameters.AddWithValue("@AttachName", AttachName);
                     command.Parameters.AddWithValue("@AttachPath", AttachPath);
                     command.Parameters.AddWithValue("@UploadDate", UploadDate);
@@ -49,34 +41,6 @@ namespace EX2.Repositories
         {
             return null;
         }
-        //public DataTable getByProject(int id)
-        //{
-        //    DataTable dt = new DataTable();
-        //    using (SqlConnection connection = new SqlConnection(_connect))
-        //    {
-        //        using (SqlCommand command = new SqlCommand())
-        //        {
-        //            command.CommandText = "Add_Attachment";
-        //            command.CommandType = CommandType.StoredProcedure;
-
-
-        //            //SqlParameter name = new SqlParameter("@AttachName", attachment.AttachName);
-        //            //SqlParameter path = new SqlParameter("@AttachPath", attachment.AttachPath);
-        //            //SqlParameter date = new SqlParameter("@UploadDate", attachment.UploadDate);
-
-        //            //command.Parameters.Add(name);
-        //            //command.Parameters.Add(path);
-        //            //command.Parameters.Add(date);
-        //            //connection.Open();
-
-        //            using (SqlDataAdapter da = new SqlDataAdapter())
-        //            {
-        //                da.Fill(dt);
-        //            }
-        //        }
-        //    }
-        //    return dt;
-        //}
 
     }
 }
